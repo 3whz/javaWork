1db38010d5c402f14516277fc8f49eb6f91284f3
Boen <1260414235@qq.com>
2020-05-19 13:56:32 +0800

    添加注释

---
+++ untitled/untitled/src/demo01/Test.java
@@ -0,0 +1,10 @@
+package demo01;
+
+import java.util.Scanner;
+public class Test {
+    public static void main(String[] args) {
+        //´´½¨
+        Scanner sc =new Scanner(System.in);
+        System.out.println("aa");
+    }
+}
